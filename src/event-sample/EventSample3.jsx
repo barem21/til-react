9c297dc980e02fb8d08c2c48e957a60f02466fae
barem21 <barem210@gmail.com>
2024-12-02 12:05:33 +0900

    [jsx 문법]

--- src/event-sample/EventSample3.jsx
+++ src/event-sample/EventSample3.jsx
@@ -25,7 +25,7 @@ const EventSample3 = () => {
       const timerName = setInterval(() => {
         //인터벌 동작
         setTimerCount(proc => proc + 1);
-      }, 1000);
+      }, 100);
 
       setTimeId(timerName); //interval Id 저장
     }
@@ -56,7 +56,7 @@ const EventSample3 = () => {
       <h1>타이머 만들기</h1>
       <div>
         <button onClick={() => timerStart()}>타이머 시작</button>
-        <button onClick={() => timerStop()}>타이머 종료</button>
+        <button onClick={() => timerStop()}>타이머 중지</button>
         <button onClick={() => timerReset()}>타이머 초기화</button>
       </div>
 
